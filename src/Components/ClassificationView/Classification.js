dcb972dc67c23628ff5412b30f59749fe4bb456d
LeonAngelit <leo63582@gmail.com>
2023-05-11 18:09:06 +0200

    added classification display validation

--- src/Components/ClassificationView/Classification.js
+++ src/Components/ClassificationView/Classification.js
@@ -11,6 +11,12 @@ const Classification = (props) => {
   const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
+    if (context.x_token && context.current_room != undefined) {
+      updateRoomData(context.current_room.id);
+    }
+  }, []);
+
+  useEffect(() => {
     setTimeout(() => {
       if (context.x_token && context.current_room != undefined) {
         updateRoomData(context.current_room.id);
@@ -68,73 +74,77 @@ const Classification = (props) => {
   return (
     <>
       {users.map((user) => {
-        return (
-          <div
-            className={
-              animate ? "user-card-wrapper animate" : "user-card-wrapper"
-            }
-            style={{
-              animationDelay: `${users.indexOf(user) * 120}ms`,
-            }}
-            key={users.indexOf(user)}
-          >
-            <article
+        if (user.countries.length == 5) {
+          return (
+            <div
+              className={
+                animate ? "user-card-wrapper animate" : "user-card-wrapper"
+              }
               style={{
-                backgroundColor: user.color,
+                animationDelay: `${users.indexOf(user) * 120}ms`,
               }}
-              className="user-card"
+              key={users.indexOf(user)}
             >
-              <div className="user-card-data">
-                <div className="user-card-info">
-                  <img
-                    src={`https://ui-avatars.com/api/${user.username}`}
-                    alt="imagen de usuario"
-                  />
-                  <p className={users.indexOf(user) == 0 ? "user-winner" : ""}>
-                    {users.indexOf(user) + 1}. {user.username}
-                  </p>
-                </div>
-                <div className="user-card-countries">
-                  {user.countries.map((country) => {
-                    return (
-                      <div
-                        key={user.countries.indexOf(country)}
-                        className="country-wrapper"
-                      >
-                        <p
-                          style={{
-                            fontWeight:
-                              country.UserCountry.winnerOption == 1
-                                ? "bold"
-                                : "100",
-                          }}
+              <article
+                style={{
+                  backgroundColor: user.color,
+                }}
+                className="user-card"
+              >
+                <div className="user-card-data">
+                  <div className="user-card-info">
+                    <img
+                      src={`https://ui-avatars.com/api/${user.username}`}
+                      alt="imagen de usuario"
+                    />
+                    <p
+                      className={users.indexOf(user) == 0 ? "user-winner" : ""}
+                    >
+                      {users.indexOf(user) + 1}. {user.username}
+                    </p>
+                  </div>
+                  <div className="user-card-countries">
+                    {user.countries.map((country) => {
+                      return (
+                        <div
+                          key={user.countries.indexOf(country)}
+                          className="country-wrapper"
                         >
-                          <span
-                            className={`fi fi-${country.code} fis country-flag`}
+                          <p
                             style={{
-                              outline:
-                                country.position == 1
-                                  ? "2px solid var(--euro-pink)"
-                                  : "none",
+                              fontWeight:
+                                country.UserCountry.winnerOption == 1
+                                  ? "bold"
+                                  : "100",
                             }}
-                          ></span>
-                          {country.UserCountry.winnerOption &&
-                          country.position == 1
-                            ? parseInt(country.points + country.points * 0.1)
-                            : country.points}
-                        </p>
-                      </div>
-                    );
-                  })}
+                          >
+                            <span
+                              className={`fi fi-${country.code} fis country-flag`}
+                              style={{
+                                outline:
+                                  country.position == 1
+                                    ? "2px solid var(--euro-pink)"
+                                    : "none",
+                              }}
+                            ></span>
+                            {country.UserCountry.winnerOption &&
+                            country.position == 1
+                              ? parseInt(country.points + country.points * 0.1)
+                              : country.points}
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
 
-              <div className="user-card-total">
-                <p>{user.total}</p>
-              </div>
-            </article>
-          </div>
-        );
+                <div className="user-card-total">
+                  <p>{user.total}</p>
+                </div>
+              </article>
+            </div>
+          );
+        }
       })}
     </>
   );
