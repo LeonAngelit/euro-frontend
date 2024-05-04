5b3499a0e35257d88b8f7e33ad3e87fd0cf64638
LeonAngelit <leo63582@gmail.com>
2024-05-04 13:56:00 +0200

    fix year

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+# syntax=docker/dockerfile:1
+
+# Comments are provided throughout this file to help you get started.
+# If you need more help, visit the Dockerfile reference guide at
+# https://docs.docker.com/engine/reference/builder/
+
+ARG NODE_VERSION=18.15.0
+
+################################################################################
+# Use node image for base image for all stages.
+FROM node:${NODE_VERSION}-alpine as base
+
+# Set working directory for all build stages.
+WORKDIR /usr/src/app
+
+
+################################################################################
+# Create a stage for installing production dependecies.
+FROM base as deps
+
+# Download dependencies as a separate step to take advantage of Docker's caching.
+# Leverage a cache mount to /root/.npm to speed up subsequent builds.
+# Leverage bind mounts to package.json and package-lock.json to avoid having to copy them
+# into this layer.
+RUN --mount=type=bind,source=package.json,target=package.json \
+    --mount=type=bind,source=package-lock.json,target=package-lock.json \
+    --mount=type=cache,target=/root/.npm \
+    npm ci --omit=dev
+
+################################################################################
+# Create a stage for building the application.
+FROM deps as build
+
+# Download additional development dependencies before building, as some projects require
+# "devDependencies" to be installed to build. If you don't need this, remove this step.
+RUN --mount=type=bind,source=package.json,target=package.json \
+    --mount=type=bind,source=package-lock.json,target=package-lock.json \
+    --mount=type=cache,target=/root/.npm \
+    npm ci
+
+# Copy the rest of the source files into the image.
+COPY . .
+RUN --mount=type=secret,id=my_env \
+    cat /run/secrets/my_env > .env.local && \
+    npm run build && \
+    rm .env.local
+
+FROM nginx:1.19.0
+WORKDIR /usr/share/nginx/html
+RUN rm -rf ./*
+COPY --from=build /usr/src/app/build .
+ENTRYPOINT [ "nginx", "-g", "daemon off;" ]
--- src/Layout.js
+++ src/Layout.js
@@ -8,79 +8,80 @@ import AppContext from "./Storage/AppContext";
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function Layout({ children }) {
-  const context = useContext(AppContext);
+	const d = new Date();
+	const context = useContext(AppContext);
 
-  async function sendTokenRequest() {
-    const salt = bcrypt.genSaltSync(12);
-    const token = bcrypt.hashSync(process.env.REACT_APP_AUTH_P, salt, null);
-    await axios
-      .get(`${process.env.REACT_APP_BASEURL}/api/eurocontest/getAuthToken`, {
-        headers: {
-          Accept: "application/json",
-          Authorization: `${token}`,
-        },
-      })
-      .then((response) => {
-        if (response.status == 200) {
-          context.setXtoken(response.data);
-        }
-      })
-      .catch((error) => {
-        return error.response.data.message;
-      });
-  }
+	async function sendTokenRequest() {
+		const salt = bcrypt.genSaltSync(12);
+		const token = bcrypt.hashSync(process.env.REACT_APP_AUTH_P, salt, null);
+		await axios
+			.get(`${process.env.REACT_APP_BASEURL}/api/eurocontest/getAuthToken`, {
+				headers: {
+					Accept: "application/json",
+					Authorization: `${token}`,
+				},
+			})
+			.then((response) => {
+				if (response.status == 200) {
+					context.setXtoken(response.data);
+				}
+			})
+			.catch((error) => {
+				return error.response.data.message;
+			});
+	}
 
-  async function updatePointRequest() {
-    await axios
-      .get(
-        `${process.env.REACT_APP_BASEURL}/api/eurocontest/countries/refresh/2023`,
-        {
-          headers: {
-            Accept: "application/json",
-            Bearer: context.x_token,
-          },
-        }
-      )
-      .then((response) => {
-        if (response.status == 200) {
-          return response.data;
-        }
-      })
-      .catch((error) => {
-        return error.response.data.message;
-      });
-  }
+	async function updatePointRequest() {
+		await axios
+			.get(
+				`${process.env.REACT_APP_BASEURL}/api/eurocontest/countries/refresh/${d.getFullYear}`,
+				{
+					headers: {
+						Accept: "application/json",
+						Bearer: context.x_token,
+					},
+				}
+			)
+			.then((response) => {
+				if (response.status == 200) {
+					return response.data;
+				}
+			})
+			.catch((error) => {
+				return error.response.data.message;
+			});
+	}
 
-  useEffect(() => {
-    sendTokenRequest();
-    setInterval(() => sendTokenRequest(), 1200000);
-  }, []);
+	useEffect(() => {
+		sendTokenRequest();
+		setInterval(() => sendTokenRequest(), 1200000);
+	}, []);
 
-  useEffect(() => {
-    let interval;
-    if (
-      context.x_token &&
-      context.user_logged?.username == process.env.REACT_APP_ADMIN
-    ) {
-      interval = setInterval(() => {
-        updatePointRequest();
-      }, 90000);
-    } else {
-      clearInterval(interval);
-    }
-  }, [context.user_logged.username]);
+	useEffect(() => {
+		let interval;
+		if (
+			context.x_token &&
+			context.user_logged?.username == process.env.REACT_APP_ADMIN
+		) {
+			interval = setInterval(() => {
+				updatePointRequest();
+			}, 90000);
+		} else {
+			clearInterval(interval);
+		}
+	}, [context.user_logged.username]);
 
-  return (
-    <>
-      <Navigation />
-      {children}
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navigation />
+			{children}
+			<Footer />
+		</>
+	);
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 export default Layout;
