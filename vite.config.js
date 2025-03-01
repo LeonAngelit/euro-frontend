fe7b501c142e9cd72251728e715d40ec0d5f6e00
LeonAngelit <leo63582@gmail.com>
2025-03-01 20:02:53 +0100

    update polyfills import

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import envCompatible from 'vite-plugin-env-compatible';
-import NodePolyfills from 'vite-plugin-node-polyfills';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,7 +12,7 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
-  plugins: [react(), envCompatible(), NodePolyfills()],
+  plugins: [react(), envCompatible(), nodePolyfills()],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser global polyfills
